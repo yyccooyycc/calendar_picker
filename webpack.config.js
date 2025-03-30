69881768587652b3f0fefbf2d350b902d80ed5c5
oliviah11 <yycc11.tt@gmail.com>
2025-03-30 15:39:32 +0800

    changes to publish to Github Pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'development', 
   entry: './src/index.js', 
   output: {
-    path: path.resolve(__dirname, 'dist'), 
+    path: path.resolve(__dirname, "build"), 
     filename: 'bundle.js', 
     clean: true, 
   },
